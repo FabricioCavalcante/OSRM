1f9055c44d0442d941cb3b93b76e46a01e8d93b1
keltonlima <86563226+keltonlima@users.noreply.github.com>
2021-12-14 13:17:21 -0300

    Correção do app Users
    Corrigido o sistema de cadastro de usuário.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from users.models import User
+
+class Listando_Requisicoes(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'placa_patrimonio')
 
 # Register your models here.
+admin.site.register(User)
--- users/models.py
+++ users/models.py
@@ -2,4 +2,4 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    tecnico = models.BooleanField(blank=True)
+    tecnico = models.BooleanField(null=True, blank=True)
