8fe18d1684f0ce058716fbe5e80d1e4fe4eecbd0
keltonlima <86563226+keltonlima@users.noreply.github.com>
2021-12-14 15:30:04 -0300

    Commit Merge

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=c:\Python\venv
+set VIRTUAL_ENV=C:\Users\arthur.almeida\desktop\dev\OSRM-1\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
